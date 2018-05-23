77d2d3c0f6213f6c475d083443cdd12d706ec65a
Joshua Suskalo <JZSuskalo@Student.FullSail.edu>
2018-05-22 22:02:40 -0400

    Make systems not crash or call with invalid entities

--- src/ECS/system.c
+++ src/ECS/system.c
@@ -31,11 +31,26 @@ void systemRun(
 	}
 
 	// Get the first component type
-	ComponentDataTable *firstComp = (ComponentDataTable *)system->componentTypes.front->data;
+	ComponentDataTable *firstComp = *(ComponentDataTable **)
+		hashMapGetKey(
+			scene->componentTypes,
+			(UUID*)system->componentTypes.front->data
+		);
+
+	UUID emptyID = {};
 
 	// For each entity in the component table
 	for (uint32 i = 0; i < firstComp->numEntries; ++i)
 	{
+		if (!strcmp(
+				emptyID.string,
+				// NOTE(Joshua): "Feels like pretty standard C to me"
+				((UUID *)(firstComp->data + i * (sizeof(UUID) + firstComp->componentSize)))->string
+			))
+		{
+			continue;
+		}
+
 		// Check to see if it has the other component types
 		int32 entityValid = 1;
 
@@ -46,7 +61,7 @@ void systemRun(
 		{
 			// Get the component to check
 			UUID *componentID = (UUID *)(*itr)->data;
-			ComponentDataTable *table = hashMapGetKey(scene->componentTypes, componentID);
+			ComponentDataTable *table = *(ComponentDataTable **)hashMapGetKey(scene->componentTypes, componentID);
 
 			// Check if the entity exists in the table
 			uint32 *entityIndex =
