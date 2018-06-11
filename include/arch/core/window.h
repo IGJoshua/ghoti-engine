5688b78550f4e5f03e0941085273cdf55b29dc60
Joshua Suskalo <JZSuskalo@Student.FullSail.edu>
2018-06-11 17:12:50 -0400

    Make a void function declaration actually void

--- include/arch/core/window.h
+++ include/arch/core/window.h
@@ -2,5 +2,5 @@
 #include "defines.h"
 
 struct GLFWwindow *initWindow(uint32 width, uint32 height, const char *title);
-int32 closeWindow();
+int32 closeWindow(void);
 int32 freeWindow(struct GLFWwindow *window);
