9ebd9230ff1c593a20e79b8738f950d3285c5aac
Pranav <psahani@student.fullsail.edu>
2018-06-13 11:10:26 +0000

    Deleted old scene files.

--- include/arch/asset_management/scene.h
+++
@@ -1,8 +0,0 @@
-// #pragma once
-// #include "defines.h"
-
-// #include "asset_management/asset_manager_types.h"
-// #include "renderer/renderer_types.h"
-
-//int32 loadScene(const char *name, Scene **scene);
-//int32 unloadScene(Scene **scene);
