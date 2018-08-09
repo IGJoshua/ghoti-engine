8e3651bacf033294e46663027f82cc4fc701206f
Joshua Suskalo <JZSuskalo@Student.FullSail.edu>
2018-08-09 13:55:57 -0400

    Remove dirty flag from rigidbodies

--- include/arch/components/rigid_body.h
+++ include/arch/components/rigid_body.h
@@ -9,8 +9,12 @@ void registerRigidBody(Scene *scene, UUID entity);
 void destroyRigidBody(RigidBodyComponent *body);
 
 void updateRigidBodyPosition(
+	Scene *scene,
+	CollisionComponent *coll,
 	RigidBodyComponent *body,
 	TransformComponent *trans);
 void updateRigidBody(
+	Scene *scene,
+	CollisionComponent *coll,
 	RigidBodyComponent *body,
 	TransformComponent *trans);
