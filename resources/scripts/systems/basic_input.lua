ddba675d0f93957d75eb819645af78d4b0d341f4
Pranav <psahani@student.fullsail.edu>
2018-08-21 07:29:21 -0500

    Renamed inputtest to basic_input.

---
+++ resources/scripts/systems/basic_input.lua
@@ -0,0 +1,34 @@
+local system = {}
+
+local input = engine.input
+local keyboard = engine.keyboard
+local mouse = engine.mouse
+local gamepad = engine.gamepad
+local C = engine.C
+
+function system.init(scene)
+  input:register("close", input.BUTTON(keyboard.ESCAPE, gamepad.buttons.guide))
+  input:register("reload", input.BUTTON(keyboard.R))
+  input:register("save", input.BUTTON(keyboard.S))
+  input:register("load_save", input.BUTTON(keyboard.Z))
+end
+
+function system.begin(scene, dt)
+  if input.close.keydown then
+    engine.C.closeWindow()
+  end
+
+  if input.reload.updated and input.reload.keydown then
+    C.reloadAllScenes()
+  end
+
+  if input.save.updated and input.save.keydown then
+    C.exportSave(nil, 0, 1)
+  end
+
+  if input.load_save.updated and input.load_save.keydown then
+    C.loadSave(1, nil)
+  end
+end
+
+return system
