3438357901a109b8c18809da863279cd20732a8d
Joshua Suskalo <JZSuskalo@Student.FullSail.edu>
2018-08-29 12:04:38 -0400

    Add lua based component data table iterator

--- resources/scripts/systems/ball_movement.lua
+++ resources/scripts/systems/ball_movement.lua
@@ -7,22 +7,12 @@ system.components = {}
 system.components[1] = "ball"
 system.components[2] = "transform"
 
-local cdtItr = ffi.new("ComponentDataTableIterator")
-local cdtItrOut = ffi.new("ComponentDataTableIterator[1]")
-
 local ballComponentID = ffi.new("UUID[1]", C.idFromName("ball"))
 local paddleComponentID = ffi.new("UUID[1]", C.idFromName("paddle"))
-local ball
 
 function system.init(scene)
-  cdtItr = C.cdtGetIterator(
-	ffi.cast(
-	  "ComponentDataTable **",
-	  C.hashMapGetData(
-		scene.ptr.componentTypes,
-		ballComponentID))[0])
-  while C.cdtIteratorAtEnd(cdtItr) == 0 do
-	ball = ffi.cast("BallComponent *", C.cdtIteratorGetData(cdtItr))
+  for itr in scene:getComponentIterator("ball") do
+	ball = itr.component
 	ball.velocity.x = math.random(-200, 200) / 10.0
 	ball.velocity.y = math.random(-200, 200) / 10.0
 
@@ -33,10 +23,6 @@ function system.init(scene)
 	if math.abs(ball.velocity.x) < 5 then
 	  ball.velocity.x = ball.velocity.x * 10
 	end
-
-	cdtItrOut[0] = cdtItr
-	C.cdtMoveIterator(cdtItrOut)
-	cdtItr = cdtItrOut[0]
   end
 end
 
@@ -44,6 +30,7 @@ local transform
 local paddle
 local paddleTransform
 local paddleUUID
+local ball
 
 function system.run(scene, uuid, dt)
   transform = scene:getComponent("transform", uuid)
@@ -67,7 +54,7 @@ function system.run(scene, uuid, dt)
 	  transform.position.y = 0
 	  transform:markDirty(scene)
 
-	  ball.velocity.x = math.random(-200, 200) / 10.0
+	  ball.velocity.x = ball.velocity.x + math.random(-200, 200) / 100.0
 	  ball.velocity.y = math.random(-200, 200) / 10.0
 
 	  if ball.velocity.x == 0 then
@@ -92,30 +79,19 @@ function system.run(scene, uuid, dt)
   end
 
   -- Check if it hit a paddle
-  cdtItr = C.cdtGetIterator(
-	ffi.cast(
-	  "ComponentDataTable **",
-	  C.hashMapGetData(
-		scene.ptr.componentTypes,
-		paddleComponentID))[0])
-  while C.cdtIteratorAtEnd(cdtItr) == 0 do
-	paddleUUID = C.cdtIteratorGetUUID(cdtItr)
-	paddle = scene:getComponent("paddle", paddleUUID)
-	paddleTransform = scene:getComponent("transform", paddleUUID)
+  for itr in scene:getComponentIterator("paddle") do
+	paddle = itr.component
+	paddleTransform = scene:getComponent("transform", itr.uuid)
 
 	-- If it hits the paddle, then go to the other side
 	if (transform.position.x < paddleTransform.position.x + paddle.bounds.x)
 	  and (transform.position.x > paddleTransform.position.x - paddle.bounds.x)
 	  and (transform.position.y < paddleTransform.position.y + paddle.bounds.y)
-	  and (transform.position.y > paddleTransform.position.y - paddle.bounds.y) then
+	and (transform.position.y > paddleTransform.position.y - paddle.bounds.y) then
 	  -- The paddle has hit the ball
 	  ball.velocity.x = -ball.velocity.x + math.random(-300, 300) / 100.0
 	  ball.velocity.y = ball.velocity.y + math.random(-300, 300) / 100.0
 	end
-
-	cdtItrOut[0] = cdtItr
-	C.cdtMoveIterator(cdtItrOut)
-	cdtItr = cdtItrOut[0]
   end
 
   transform.position.x = transform.position.x + ball.velocity.x * dt
