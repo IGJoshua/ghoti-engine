1b91458082fbc3aa379e1d1a650cbfd031315c2b
Pranav <psahani@student.fullsail.edu>
2018-10-23 12:47:11 -0500

    Updated the lua definition for the JointType enumerator.

--- resources/scripts/cdefs/physics.lua
+++ resources/scripts/cdefs/physics.lua
@@ -6,13 +6,6 @@ typedef void *dSpaceID;
 typedef void *dGeomID;
 typedef void *dJointID;
 
-typedef enum joint_type_e
-{
-	JOINT_TYPE_BALL_SOCKET = 0,
-	JOINT_TYPE_HINGE,
-	JOINT_TYPE_SLIDER
-} JointType;
-
 typedef struct ray_collision_t
 {
     bool hasContact;
--- resources/scripts/components/joint_information.lua
+++ resources/scripts/components/joint_information.lua
@@ -1,4 +1,13 @@
 ffi.cdef[[
+typedef enum joint_type_e
+{
+	JOINT_TYPE_BALL_SOCKET = 0,
+	JOINT_TYPE_HINGE,
+	JOINT_TYPE_HINGE2,
+	JOINT_TYPE_SLIDER,
+	JOINT_TYPE_BALL_SOCKET2
+} JointType;
+
 typedef struct joint_information_component_t
 {
 	JointType type;
